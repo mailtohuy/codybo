9631954a870c8873ef5aa9825ee6104db384e625
mailtohuy <mailtohuy@gmail.com>
2016-05-26 15:58:12 -0400

    switched to send hello on request of /

--- web.js
+++ web.js
@@ -1,4 +1,7 @@
 var express = require('express')
 var server = express()
-server.use(express.static(__dirname + '/public'));
+//server.use(express.static(__dirname + '/public'));
+server.get('/', function(req,res) {
+	res.send("Hello!");
+});
 server.listen(80);
