4196d009ce9e2e0983fece3af552096e69a5c2be
mailtohuy <mailtohuy@gmail.com>
2016-05-26 16:48:43 -0400

    fixed express.static in web.js, app.js path in index.html and comment error in app.js

--- public/app.js
+++ public/app.js
@@ -1,12 +1,13 @@
 App = Ember.Application.create();
 App.Router.map(function() {
-            // put your routes here });  
-            App.IndexRoute = Ember.Route.extend({
-                model: function() {
-                    var url = 'https://lcboapi.com/stores/411/products?access_key=MDoxMzM1YjdiYS0yMjlmLTExZTYtOTEyYS04Zjc5ZTE4MmM5ZTE6dGF5dDZSZzZ2ZHF0ajN4ZWE5UXQxYXQyNHpja1laTjZleDBs&where=has_limited_time_offer&q=wine&per_page=100';
-                    var products = Ember.$.getJSON(url).then(function(data) {
-                        return data.result;
-                    });
-                    return products;
-                }
-            });
+
+});  
+App.IndexRoute = Ember.Route.extend({
+    model: function() {
+        var url = 'https://lcboapi.com/stores/411/products?access_key=MDoxMzM1YjdiYS0yMjlmLTExZTYtOTEyYS04Zjc5ZTE4MmM5ZTE6dGF5dDZSZzZ2ZHF0ajN4ZWE5UXQxYXQyNHpja1laTjZleDBs&where=has_limited_time_offer&q=wine&per_page=100';
+        var products = Ember.$.getJSON(url).then(function(data) {
+            return data.result;
+        });
+        return products;
+    }
+});
