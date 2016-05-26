9e0d0c2e865180d4bf2ed13a0399fd458378e35d
mailtohuy <mailtohuy@gmail.com>
2016-05-26 16:53:35 -0400

    changed port to 8080. Port 80 does not work

--- web.js
+++ web.js
@@ -1,7 +1,6 @@
 var express = require('express')
 var server = express()
-//server.use(express.static(__dirname + '/public'));
-server.get('/', function(req,res) {
-	res.send("Hello!");
-});
-server.listen(80);
+
+server.use(express.static('public'));
+
+server.listen(8080);
