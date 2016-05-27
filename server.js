b927811a9c781bbabfff41258bac690cc3a84126
mailtohuy <mailtohuy@gmail.com>
2016-05-27 18:48:07 -0400

    fixed typo

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ var cors = require('cors')
 var express = require('express')
 var server = express()
 
-server.use(cors);
+server.use(cors)
 server.set('PORT', (process.env.PORT || 5000));
 
 
@@ -11,4 +11,4 @@ server.use(express.static('public'));
 var port = server.get('PORT');
 server.listen(port, function() {
 	console.log('codybo is running on port', port);
-});se
+});
