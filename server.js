99f63919218dfe5bad3c1d50d62fb814c4739401
mailtohuy <mailtohuy@gmail.com>
2016-05-27 18:46:27 -0400

    added server.use(cors) to enable CORS in Express

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ var cors = require('cors')
 var express = require('express')
 var server = express()
 
+server.use(cors);
 server.set('PORT', (process.env.PORT || 5000));
 
 
@@ -10,4 +11,4 @@ server.use(express.static('public'));
 var port = server.get('PORT');
 server.listen(port, function() {
 	console.log('codybo is running on port', port);
-});
+});se
