5f9a6016e364105c2bf354679f6b7b2773fbd4ff
mailtohuy <mailtohuy@gmail.com>
2016-05-28 02:33:27 -0400

    registered a new LCBO API Web key that is tied to heroku

--- public/app.js
+++ public/app.js
@@ -5,7 +5,7 @@ App.Router.map(function() {
 
 App.IndexRoute = Ember.Route.extend({
     model: function() {
-        var url = 'https://lcboapi.com/stores/411/products?access_key=MDowOTQ3OGUzNi0yNDlkLTExZTYtYjM1Yi00ZjQxODkxMDU1MWY6Umt5UnhOc2dZZ01QOVZhbXpGbGFDb05heHZwODNwcG9wQmt0&where=has_limited_time_offer&q=wine&per_page=100';
+        var url = 'https://lcboapi.com/stores/411/products?access_key=MDplMGI1YTk5OC0yNDlkLTExZTYtOWViYy0wMzVmNTdmYjRmMzU6QngxdFc0Qlo3OEFhSDdHMnU0aVpBYUNFSHVSYWRzdkp2Yzdh&where=has_limited_time_offer&q=wine&per_page=100';
         var products = Ember.$.getJSON(url).then(function(data) {
             return data.result;
         });
