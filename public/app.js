5a283f43a0e07bdd4cc62b409e657fe7c17fb6ff
mailtohuy <mailtohuy@gmail.com>
2016-05-28 02:27:27 -0400

    changed LCBO API key to Backend type

--- public/app.js
+++ public/app.js
@@ -1,10 +1,11 @@
 App = Ember.Application.create();
 App.Router.map(function() {
 
-});  
+});
+
 App.IndexRoute = Ember.Route.extend({
     model: function() {
-        var url = 'https://lcboapi.com/stores/411/products?access_key=MDoxMzM1YjdiYS0yMjlmLTExZTYtOTEyYS04Zjc5ZTE4MmM5ZTE6dGF5dDZSZzZ2ZHF0ajN4ZWE5UXQxYXQyNHpja1laTjZleDBs&where=has_limited_time_offer&q=wine&per_page=100';
+        var url = 'https://lcboapi.com/stores/411/products?access_key=MDowOTQ3OGUzNi0yNDlkLTExZTYtYjM1Yi00ZjQxODkxMDU1MWY6Umt5UnhOc2dZZ01QOVZhbXpGbGFDb05heHZwODNwcG9wQmt0&where=has_limited_time_offer&q=wine&per_page=100';
         var products = Ember.$.getJSON(url).then(function(data) {
             return data.result;
         });
