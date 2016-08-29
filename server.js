f5e33c0c1ac9e29d15c14fb96dfeef45b1aa7c92
mailtohuy <mailtohuy@gmail.com>
2016-08-29 00:44:46 -0400

    removed JSON.toString to getStoresNearby

--- server.js
+++ server.js
@@ -20,7 +20,7 @@ server.get("/lcbo/:storeId", function(req,res) {
 
 server.get("/lcbo-nearby", function(req,res) {
 	lcbo.getStoresNearby(req.query.lat,req.query.lon)
-	.then((json) => res.send(JSON.toString(json)));
+	.then((json) => res.send(json));
 });
 
 var port = server.get('PORT');
