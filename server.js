34bc63fb1f99b8e7929e4f525616ee38dfe81bab
mailtohuy <mailtohuy@gmail.com>
2016-08-28 23:04:45 -0400

    add lcbo service

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 var express = require('express')
 var bodyParser = require("body-parser");
+var lcbo = require('./codybo.js');
 var server = express()
 
 server.set('PORT', (process.env.PORT || 5000));
@@ -8,17 +9,12 @@ server.set('PORT', (process.env.PORT || 5000));
 server.use(express.static(__dirname + '/public'));
 server.use(bodyParser.json());
 
-/*  "/contacts"
- *    GET: finds all contacts
- *    POST: creates a new contact
- */
-
-server.get("/news/list", function(req, res) {
-	res.send('Hello World!');
-});
-
 server.get("/echo/:text", function(req, res) {
 	res.send(req.params.text);
+})
+
+server.get("/lcbo/:storeId", function(req,res) {
+	lcbo.getSalesAtStore(req.params.storeId).then((json) => res.send(json));
 })
 
 var port = server.get('PORT');
