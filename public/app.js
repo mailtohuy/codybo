669eafa0a9e81eb302a3873608481eda10680b3f
mailtohuy <mailtohuy@gmail.com>
2016-08-28 23:09:46 -0400

    modified app.js to use internal lcbo service

--- public/app.js
+++ public/app.js
@@ -5,9 +5,9 @@ App.Router.map(function() {
 
 App.IndexRoute = Ember.Route.extend({
     model: function() {
-        var url = 'https://lcboapi.com/stores/411/products?access_key=MDplMGI1YTk5OC0yNDlkLTExZTYtOWViYy0wMzVmNTdmYjRmMzU6QngxdFc0Qlo3OEFhSDdHMnU0aVpBYUNFSHVSYWRzdkp2Yzdh&where=has_limited_time_offer&q=wine&per_page=100';
+        var url = 'https://codybo.herokuapp.com/lcbo/411';
         var products = Ember.$.getJSON(url).then(function(data) {
-            return data.result;
+            return data;
         });
         return products;
     }
