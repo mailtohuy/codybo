2b4b817c381f37a1835ebe4355df4ea5dcfd0b9a
mailtohuy <mailtohuy@gmail.com>
2016-09-04 22:53:26 -0400

    added service to handle groupme messages

--- server.js
+++ server.js
@@ -1,12 +1,15 @@
 var express = require('express')
 var bodyParser = require("body-parser");
 var lcbo = require('./codybo.js');
+
+
 var server = express()
 
 server.set('PORT', (process.env.PORT || 5000));
 
 
 server.use(express.static(__dirname + '/public'));
+
 server.use(bodyParser.json());
 
 server.get("/echo/:text", function(req, res) {
@@ -37,6 +40,46 @@ server.get("/lcbo-product/:name", function(req,res) {
 server.get("/lcbo-inventory/:productId", function(req,res) {	
 	lcbo.lookUpInventory(req.params.productId)
 	.then((json) => res.send(json));
+});
+
+server.post("/groupme", function(req,res) {
+	var db = require('mongoose');
+	db.Promise = global.Promise;
+	db.connect('mongodb://writer:writer@ds013456.mlab.com:13456/heroku_9lwl8gdd');
+	db.connection.on('error', console.error.bind(console, 'connection error!'));
+	db.connection.once('open', function() {
+
+		/* Create schema */
+		var messageSchema = new db.Schema({
+			attachments: [String],
+			avatar_url: String,
+			created_at: Number,
+			group_id: Number,
+			id: Number,
+			name: String,
+			sender_id: String,
+			sender_type: String,
+			source_guid: String,
+			system: Boolean,
+			text: String,
+			user_id: Number
+		});
+		
+		var ChatMessage = db.model('ChatMessage', messageSchema, 'GroupMe');
+
+		/* Save message to database */
+		var message = new ChatMessage(req.body);
+
+		message.save(function (err) {
+			if (err) {
+				console.error(err);
+		  	} 
+
+			/* Close connection */
+		  	db.connection.close()
+		}); //message.save
+
+	});		
 });
 
 var port = server.get('PORT');
