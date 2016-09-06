085ee33164a2f9064f13bd7c9d9d32dae83b2baa
mailtohuy <mailtohuy@gmail.com>
2016-09-05 22:31:06 -0400

    use dispatcher for groupme service

--- server.js
+++ server.js
@@ -54,6 +54,42 @@ server.get("/lcbo-inventory", function(req,res) {
 	}
 });
 
+
+(function() {
+	/* register the handlers for groupme service */
+	gm.dispatcher.addKeywords(['find', 'near', 'info']);
+
+	
+	var formatStoreResults = function (results) {
+
+		var subset = results.splice(0,5); //limit response to 5 records.
+
+		subset.map(function (store) {
+			return store.name + ', quantity: ' +  store.quantity;
+		}).join('.\n'); 
+
+		return txt;
+	};
+
+	/* add handler for 'find pid near geo' */
+	gm.dispatcher.registerHandler(['find', 'near'], function(pid, geo) {
+		return lcbo.lookUpInventoryNearAddress(pid, geo)
+		.then(function(results) {
+			var txt = formatStoreResults(results);
+			gm.postMessage(txt);
+		}); 
+	}); // function(pid,geo)
+
+	/* add handler for 'info' */
+	gm.dispatcher.registerHandler(['info'], function(ignore) {
+		gm.postMessage('find <name>: lists products with matching name\n
+						find <pid> near <address>: nearest stores where product is in stock');
+	});
+
+
+
+})();
+
 server.post("/groupme", function(req,res) {
 
 	console.log(JSON.stringify(req.body));
@@ -62,28 +98,31 @@ server.post("/groupme", function(req,res) {
 	gm.receiveMessage(req.body);
 
 	/* Dispatch command */
-	var cmd = req.body.text.toLowerCase().split(' '); //TODO: dissect command by key words, e.g "find (smirnoff vodka) near (m3n 2a7)" 
-	if (cmd[0] == 'find') {
-		lcbo.findProduct(cmd[1])
-		.then(function(results){
-			var txt = results.map(function(p) {
-				return p.id + ', ' + p.name + ', $' + p.price_in_cents/100 ;
-			}).join('.\n'); //TODO: limit response to 3 records.
+	var cmd = req.body.text.toLowerCase();
+	gm.dispatch(cmd);
 
-			gm.postMessage(txt);
-		});
-	}; // if cmd = 'find'
+	// var cmd = req.body.text.toLowerCase().split(' '); //TODO: dissect command by key words, e.g "find (smirnoff vodka) near (m3n 2a7)" 
+	// if (cmd[0] == 'find') {
+	// 	lcbo.findProduct(cmd[1])
+	// 	.then(function(results){
+	// 		var txt = results.map(function(p) {
+	// 			return p.id + ', ' + p.name + ', $' + p.price_in_cents/100 ;
+	// 		}).join('.\n'); //TODO: limit response to 3 records.
 
-	if (cmd[0] == 'near') {
-		lcbo.lookUpInventoryNearAddress(cmd[1], cmd[2])
-		.then(function(results) {
-			var txt = results.map(function(s) {
-				return s.name + ', quantity: ' +  s.quantity;
-			}).join('.\n'); //TODO: limit response to 3 records.
+	// 		gm.postMessage(txt);
+	// 	});
+	// }; // if cmd = 'find'
 
-			gm.postMessage(txt);
-		}) 
-	} // if cmd = 'near'
+	// if (cmd[0] == 'near') {
+	// 	lcbo.lookUpInventoryNearAddress(cmd[1], cmd[2])
+	// 	.then(function(results) {
+	// 		var txt = results.map(function(s) {
+	// 			return s.name + ', quantity: ' +  s.quantity;
+	// 		}).join('.\n'); //TODO: limit response to 3 records.
+
+	// 		gm.postMessage(txt);
+	// 	}) 
+	// } // if cmd = 'near'
 });
 
 var port = server.get('PORT');
