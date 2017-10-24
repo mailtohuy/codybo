186486a8d77c826431dc10faacd3d3f3022e1ad9
mailtohuy <mailtohuy@gmail.com>
2017-10-23 21:48:17 -0400

    added lcbo data collection script

---
+++ lcbo/analyze.js
@@ -0,0 +1,72 @@
+const fs = require('fs');
+const _ = require('underscore');
+
+function load_inventory(file) {
+	return new Promise((resolve,reject) => {
+		fs.readFile(file,'utf8',(err,raw)=>{
+			if (err) throw err;
+			try {
+				let data = _.chain(raw.split('\n'))
+					.map(row => _.object(['product_id','store_id','quantity','price','updated_on'], JSON.parse(row)))
+//		 			.tap(console.log)
+					.value();
+				resolve(data);
+			} catch (e) {
+				reject(e);
+			}
+		});
+	});
+}
+
+load_inventory('./inventories.csv')
+.then(data=>{
+	let products_by_store = _.chain(data)
+			.groupBy('store_id')
+			.value();
+			
+	let all_stores = _.keys(products_by_store);
+	
+	/* Returns the product with most quantity at a store */
+	//TODO: make this a local maximum, not global, by giving a time frame
+	let get_most_stocked = function(store_id) {
+		let products = products_by_store[store_id];
+		let most_stock = _.max(products, product=>product['quantity']);
+		return most_stock;
+	}
+
+	let most_stocked = _.chain(all_stores)
+	.map(get_most_stocked)
+	.value();
+	
+	// most_stocked.map(product => console.log(product['product_id'] + ' @ ' + product['store_id']))
+	
+	/* Returns the inventory history of a product at a store */
+	let get_inventory_history = function (store_id, product_id) {
+		let products = products_by_store[store_id];	
+		let inventory_history = _.chain(products)
+			.filter(product => product['product_id'] === product_id)
+//			.map(product => _.pick(product, 'quantity', 'updated_on'))
+			.value();
+
+		return inventory_history;
+	}
+
+	
+	let most_stock_history = _.chain(all_stores)
+	                          .map(get_most_stocked)
+                              .map(product => get_inventory_history(product['store_id'],product['product_id'])) 
+                              .value();
+//	console.log(most_stock_history);
+
+	exports.ps = products_by_store;
+	exports.ss = all_stores;
+	exports._ = _;
+
+	
+})
+
+/*
+Done - 1. what are the most stocked products at store #?
+2. what products were sold yesterday at store #?
+
+*/
---
+++ lcbo/collect.js
@@ -0,0 +1,101 @@
+const https = require('https');
+const _ = require('underscore');
+const fs = require('fs');
+
+const product_url = 'https://lcboapi.com/products?access_key=MDpkNzE1NTI2ZS0xOWUyLTExZTYtOGVlMi03N2U2MGFjMTAzMjY6QVdzWGpYUFQweW9uejFmRUZjYkNzcVhicE5UWktXQWdna0cz&per_page=50&store_id=';
+const file_base = '/projects/project1';
+//const out_file = `${file_base}/${(new Date())/1E3|0}.csv`;
+const out_file = `${file_base}/inventories.csv`;
+
+function get(url) {
+    return new Promise((resolve, reject)=>{
+        https.get(url, (res)=> {  
+            res.setEncoding('utf8');
+            let rawData = '';
+            res.on('data', (chunk) => { rawData += chunk; });
+            res.on('end', () => {
+                try {
+                    let data;                      
+                    data = JSON.parse(rawData);              
+//                    console.log(`[INFO] got ${data.result.length} records from ${url.slice(url.indexOf('store_id'))}`) ;
+                    resolve(data);   
+                } catch (e) {
+                    console.log(`[ERR] no JSON: ${url.slice(url.indexOf('store_id'))}`);
+                    resolve([]);
+                }                                 
+            });
+    
+        }).on('error', (err) => {
+            console.log(`[ERROR] get: ${url}\n${err}`);
+            resolve([]);
+        });
+});
+}
+
+function getAll(base_url) {
+    return new Promise((resolve, reject)=>{        
+        get(base_url)
+        .then((res)=>{
+            
+            let pages =_.range(res.pager.total_pages) /* NOTE change this to 1 for testing */
+                        .map(index => base_url + `&page=${index+1}`)
+                        .map(url => get(url));                        
+            
+            Promise.all(pages)
+            .then(all_pages=>{
+                let records = all_pages
+                              .filter(page => page.result != undefined)  // filter out empty page, i.e. without 'result' section
+                              .reduce((a,b)=>a.concat(b.result), []); // extract the 'result' section of each page                      
+//                console.log(`[INFO] getAll: ${records.length} records in total`);
+                resolve(records); /* Note: 'return' does not work! */
+            });
+        }) /* get(url).then */
+        .catch(err=>reject(err));    
+    });
+}
+
+
+function getProductsByStore(storeNumber) {    
+    let base_url = product_url + storeNumber;
+    
+    console.log(`[INFO] getProductsByStore: Fetching from store ${storeNumber}`);
+    
+    return getAll(base_url)
+    .then(products => {
+        console.log(`[INFO] getProductsByStore: Store ${storeNumber} has ${products.length} products`);
+        var content = _.chain(products)
+                       .map(product => _.chain(product)
+                                 .defaults({ 'store_id': storeNumber })  /* add store number */  
+                                 .pick('id', 'store_id', 'quantity', 'price_in_cents', 'updated_on') /* arrange in this order */
+                                 .values() /* convert object to array */
+                                 .value())
+                        .value();
+        return content;
+    }); /* getAll */
+}
+
+function saveToFile(content, file_name) {
+    let data = '\n' + content
+              .map(row=>JSON.stringify(row))
+              .join('\n');
+
+    fs.appendFile(file_name, data, 'utf8', (err) => {
+        if (err) throw err;
+        console.log(`[INFO] saveToFile: ${file_name} done!`);
+    });       
+}
+
+Promise.all(
+    [ 10, 217, 1, 38, 187, 164, 355, 149, 367, 346 ]
+    .map(getProductsByStore)
+).then(all_products => saveToFile(all_products.reduce((a,b)=>a.concat(b), []), out_file));
+
+/*
+ * TODO:
+ * 1. Add ability to re-download failed pages
+ * 
+ * DONE:
+ * 1. Filter out empty pages, which are promise rejections in get. DONE
+ * 2. Fix async file write: by waiting for all downloading. DONE
+ * 3. Fix the merging of products from all stores. DONE
+ */
