655b21e37f8c564efd1af5592e3881313ab7fb21
Huy <mailtohuy@gmail.com>
2017-10-27 07:37:22 -0400

    modified list of stores

--- lcbo/collect.js
+++ lcbo/collect.js
@@ -87,7 +87,7 @@ function saveToFile(content, file_name) {
 
 Promise.all(
     //[ 10, 217, 1, 38, 187, 164, 355, 149, 367, 346 ]
-    [ 38, 187 ]
+    [ 10 ]
     .map(getProductsByStore)
 ).then(all_products => saveToFile(all_products.reduce((a,b)=>a.concat(b), []), out_file));
 
