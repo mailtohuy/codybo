c9638779a3edc57e250e107a87d370ae49660691
mailtohuy <mailtohuy@gmail.com>
2017-10-26 22:10:50 -0400

    do git commit after checking

--- lcbo/daily.sh
+++ lcbo/daily.sh
@@ -3,11 +3,10 @@ before=$(wc -l inventories.csv)
 echo "$before"
 node collect.js > /dev/null
 sort inventories.csv|uniq > t.csv
-rm inventories.csv
+rm -f inventories.csv
 mv t.csv inventories.csv
 after=$(wc -l inventories.csv)
 echo "$after"
 if [ "$before" != "$after" ]; then
-	echo "Inventories updated"
-#	git commit inventories.csv -m "Updated inventories on $(date)"
+	git commit inventories.csv -m "Updated inventories on $(date)"
 fi
