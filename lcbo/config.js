2ea362865cc0589cd95db3800b98ccbc55b951ed
Cody <codemonk08@gmail.com>
2017-10-27 21:31:51 +0000

    enabled all stores

--- lcbo/config.js
+++ lcbo/config.js
@@ -4,5 +4,5 @@ module.exports = {
   access_key: access_key,
   product_url: `https://lcboapi.com/products?access_key=${access_key}&per_page=100&store_id=`,  
   out_file: `${file_base}/inventories.csv`,
-  stores: [  568 ] //, 512, 2, 360, 186, 408, 16, 10, 217, 1, 38, 187, 164, 355, 149, 367, 346 ]
+  stores: [  568 , 512, 2, 360, 186, 408, 16, 10, 217, 1, 38, 187, 164, 355, 149, 367, 346 ]
 }
