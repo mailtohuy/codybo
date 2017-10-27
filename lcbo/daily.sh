ab7f5c299f56f850bf8fb4c05aa4eade382d626c
Huy <mailtohuy@gmail.com>
2017-10-27 07:34:33 -0400

    added download size

--- lcbo/daily.sh
+++ lcbo/daily.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 before=$(wc -l inventories.csv)
+before_size=$(ls -l inventories.csv)
 echo "$before"
 cp inventories.csv inventories.csv.bk
 node collect.js 2> /dev/null
+after_size=$(ls -l inventories.csv)
 sort inventories.csv|uniq > t.csv
 mv t.csv inventories.csv
 after=$(wc -l inventories.csv)
@@ -14,3 +16,5 @@ else
 	echo "No change in inventories!"
 fi
 rm -f inventories.csv.bk
+echo "$before_size"
+echo "$after_size"
