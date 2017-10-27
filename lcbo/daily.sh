d4a01def6c1d4bee14c8bd361de668922490e380
mailtohuy <mailtohuy@gmail.com>
2017-10-26 22:07:43 -0400

    fixed string comparison in daily.js

--- lcbo/daily.sh
+++ lcbo/daily.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 before=$(wc -l inventories.csv)
-echo "$before" 
-node collect.js > /dev/null 
+echo "$before"
+node collect.js > /dev/null
 sort inventories.csv|uniq > t.csv
 rm inventories.csv
 mv t.csv inventories.csv
 after=$(wc -l inventories.csv)
-echo "$after" 
-if [ "$before" == "$after" ]; then
+echo "$after"
+if [ "$before" != "$after" ]; then
 	echo "Inventories updated"
-	git commit inventories.csv -m "Updated inventories on $(date)" 
+#	git commit inventories.csv -m "Updated inventories on $(date)"
 fi
