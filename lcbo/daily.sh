b67df05b7399c4b5552e3d4b0598d1dd9887b94b
mailtohuy <mailtohuy@gmail.com>
2017-11-04 16:13:07 -0400

    added awareness for OS  Linux vs Darwin

--- lcbo/daily.sh
+++ lcbo/daily.sh
@@ -1,11 +1,18 @@
 #!/bin/bash
+os=$(uname)
 
 #echo current date
 date
 
 # count lines and size of inventory before update
+
 before=$(wc -l inventories.csv)
-before_size=$(stat -c '%s' inventories.csv)
+
+if [ "$(os)" == "Linux" ]; then
+	before_size=$(stat -c '%s' inventories.csv 2>/dev/null)
+elif [[ "$(os)" == "Darwin" ]]; then
+	before_size=$(stat -f '%z' inventories.csv 2>/dev/null)
+fi
 
 # make a back up of inventory in case things fail
 cp inventories.csv inventories.csv.bk
@@ -14,7 +21,14 @@ cp inventories.csv inventories.csv.bk
 node collect.js 2> /dev/null
 
 # get size of inventory right after update to calculate size of downloaded data
-after_size=$(stat -c '%s' inventories.csv)
+
+if [ "$(os)" == "Linux" ]; then
+	after_size=$(stat -c '%s' inventories.csv 2>/dev/null)
+elif [[ "$(os)" == "Darwin" ]]; then
+	after_size=$(stat -f '%z' inventories.csv 2>/dev/null)
+fi
+
+
 
 # remove duplicates from inventory
 sort inventories.csv|uniq > t.csv
@@ -23,13 +37,15 @@ mv t.csv inventories.csv
 # count lines of inventory after update
 after=$(wc -l inventories.csv)
 
+
 # commit the new inventory to github
 if [ "$before" != "$after" ]; then
 		echo "No of products before update: $before"
 		echo "No of products after update: $after"
 		echo "Inventories updated!"
-		#git commit inventories.csv -m "[daily.sh] Updated inventories on $(TZ='America/Toronto' date)" 2>&1 >/dev/null
-		#git push origin master 2>&1 >/dev/null
+
+		git add inventories.csv
+		git commit -m "[daily.sh] Updated inventories on $(TZ='America/Toronto' date)" 2>&1 >/dev/null
 		if [ $? -eq 0 ]
 		then
 			   echo "Inventories saved to github !"
@@ -48,4 +64,4 @@ else
 fi
 
 # display size of downloaded data
-echo "Mobile data used: $(( $after_size - $before_size )) bytes" 
+echo "Mobile data used: $(( $after_size - $before_size )) bytes"
